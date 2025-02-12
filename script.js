567ede9ef3a33ead90633c1f1def183c0af078fe
MightyTuniek <88948412+MightyTuniek@users.noreply.github.com>
2025-02-13 00:33:46 +0100

    bug fix
    the clock goes over 1000 sometimes so fixed it by setting the number to the remainder from dividing by 1000

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ function swatchTime() {
 
     let bmt_hours = utc_hours + 1; // Convert to Biel Mean Time (BMT)
     let swatch = (((bmt_hours * 60 + minutes) * 60 + seconds) * 1000 + milliseconds) / 86400; // Swatch beats (@000 to @999)
+    
+    swatch %= 1000;
 
     // Update all elements with class "swatch"
     document.querySelectorAll('.swatch').forEach(el => {
         el.innerHTML = `@${swatch.toFixed(2)}`;
     });
+
+    
 }
 
 // Update every 10 milliseconds for smooth display
